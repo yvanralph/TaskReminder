9ae7dd09865d19936133b9a6d091ae569c50ca96
yvanralph <ishimweralph@gmail.com>
2025-08-26 14:57:41 +0200

    creation of mainjs file, required packages npm and menu display function

---
+++ main.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+// function for main menu
+function getMenu(){
+    console.log("******** Welcome To TaskReminder *******");
+    console.log("");
+    console.log("1. Today Tasks");
+    console.log("2. Schedule Task");
+    console.log("3. View Tasks");
+    console.log("4. Exit");
+    console.log("");
+}
+
