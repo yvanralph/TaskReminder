21a8fb2b902071af130f0c6c5f8af71e0584f885
yvanralph <ishimweralph@gmail.com>
2025-08-26 15:20:26 +0200

    creatin of main function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 
 // function for main menu
 function getMenu(){
-    console.log("******** Welcome To TaskReminder *******");
+    console.log("*** Welcome To TaskReminder ***");
     console.log("");
     console.log("1. Today Tasks");
     console.log("2. Schedule Task");
@@ -11,3 +11,26 @@ function getMenu(){
     console.log("");
 }
 
+// function main(){
+//     getMenu();
+//     let choice;
+//     while (true){
+//         choice = prompt("Enter your choice [1-4]: ");
+//         if (choice === "1"){
+//             console.log("Today's Section+++++++");
+//         }
+//         else if(choice === "2"){
+//             console.log("Schedule section ++++++++++++++");
+//         }
+//         else if(choice === "3"){
+//             console.log("view Section+++++++++++++++++++");
+//         }
+//         else if(choice === "4"){
+//             return;
+//         }
+//         else{
+//             console.log("Invalid Input!!  Please Try Again");
+//         }
+//     }
+// }
+// main();
